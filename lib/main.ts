5ed586e73fcbf9eb3f9b9a99226a6bec3bbfb273
Mishieck <mishieckmwale@gmail.com>
2023-01-03 21:20:51 +0200

    feat: import logical components in main file

---
+++ lib/main.ts
@@ -0,0 +1,7 @@
+import Empty from "./empty";
+import Maybe from "./maybe";
+import Either from "./either";
+import Any from "./any";
+import Some from "./some";
+
+export { Empty, Maybe, Either, Any, Some };
