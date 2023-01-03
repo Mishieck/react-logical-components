17d31b09d3192061691300def2d3d79d57f0c86e
Mishieck <mishieckmwale@gmail.com>
2023-01-03 21:19:08 +0200

    feat: change component name of Any

--- lib/any.tsx
+++ lib/any.tsx
@@ -1,15 +1,15 @@
 import React, { Children } from "react";
 
-interface EitherPropsInterface {
+interface AnyPropsInterface {
   display: number;
   children: React.ReactNode;
 }
 
-const Either: React.FC<EitherPropsInterface> = (props) => {
+const Any: React.FC<AnyPropsInterface> = (props) => {
   const { display, children } = props;
   // TODO: Handler index out of bounds
   return Children.toArray(children)[display] as React.ReactElement;
 };
 
-export default Either;
-export type { EitherPropsInterface };
+export default Any;
+export type { AnyPropsInterface };
