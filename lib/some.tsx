3e470731c8e07b85242999411c6550dceff3dbb7
Mishieck <mishieckmwale@gmail.com>
2023-01-03 21:18:48 +0200

    feat: change component name of Some

--- lib/some.tsx
+++ lib/some.tsx
@@ -1,16 +1,16 @@
 import React, { Children } from "react";
 
-interface EitherPropsInterface {
+interface SomePropsInterface {
   display: Array<number>;
   children: React.ReactNode;
 }
 
-const Either: React.FC<EitherPropsInterface> = (props) => {
+const Some: React.FC<SomePropsInterface> = (props) => {
   const { display, children } = props;
   const elements: Array<React.ReactNode> = Children.toArray(children);
   // TODO: Handler index out of bounds
   return <>{display.map((index) => elements[index])}</>;
 };
 
-export default Either;
-export type { EitherPropsInterface };
+export default Some;
+export type { SomePropsInterface };
