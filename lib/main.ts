26e9fe71ebd2977ffb15c7263859920603056a65
Mishieck <mishieckmwale@gmail.com>
2023-01-04 10:57:54 +0200

    feat: add ForEach to main file

--- lib/main.ts
+++ lib/main.ts
@@ -3,5 +3,6 @@ import Maybe from "./maybe";
 import Either from "./either";
 import Any from "./any";
 import Some from "./some";
+import ForEach from "./for-each";
 
-export { Empty, Maybe, Either, Any, Some };
+export { Empty, Maybe, Either, Any, Some, ForEach };
