bb067d1d8b800745231f1579f4e8767ab1b5b861
Mishieck <mishieckmwale@gmail.com>
2023-01-06 19:57:49 +0200

    fix: use correct file paths for components in main file

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,8 @@
-import Empty from "./empty";
-import Maybe from "./maybe";
-import Either from "./either";
-import Any from "./any";
-import Some from "./some";
-import ForEach from "./for-each";
+import Empty from "./empty/empty";
+import Maybe from "./maybe/maybe";
+import Either from "./either/either";
+import Any from "./any/any";
+import Some from "./some/some";
+import ForEach from "./for-each/for-each";
 
 export { Empty, Maybe, Either, Any, Some, ForEach };
