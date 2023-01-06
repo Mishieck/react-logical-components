49ff6b22707b95f782f0a9e6f19b486d9c8f4629
Mishieck <mishieckmwale@gmail.com>
2023-01-06 20:04:37 +0200

    feat: export types from main file

--- lib/main.ts
+++ lib/main.ts
@@ -6,3 +6,12 @@ import Some from "./some/some";
 import ForEach from "./for-each/for-each";
 
 export { Empty, Maybe, Either, Any, Some, ForEach };
+export type { MaybePropsInterface } from "./maybe/maybe";
+export type { EitherPropsInterface } from "./either/either";
+export type { AnyPropsInterface } from "./any/any";
+export type { SomePropsInterface } from "./some/some";
+
+export type {
+  ForEachPropsInterface,
+  RenderComponentType
+} from "./for-each/for-each";
